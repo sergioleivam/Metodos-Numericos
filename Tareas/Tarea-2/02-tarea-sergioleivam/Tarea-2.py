8651ce9d9c5aa6e2aaecc261cf259139cdbac1ba
sergio <sergio.leivam@hotmail.com>
2016-09-26 16:56:19 -0300

    git se volvio loco

--- Tareas/Tarea-2/02-tarea-sergioleivam/Tarea-2.py
+++ Tareas/Tarea-2/02-tarea-sergioleivam/Tarea-2.py
@@ -1,7 +1,6 @@
 
 # coding: utf-8
 
-# In[1]:
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,8 +8,6 @@ import matplotlib.cm as cm
 import scipy.optimize
 get_ipython().magic(u'matplotlib inline')
 
-
-# In[2]:
 
 ### codigo de contour, subido por el profe 
 ### solo grafica el nivel cero, para mi facilidad
@@ -36,9 +33,9 @@ plt.title('Funciones F1 y F2 en nivel cero')
 ######
 
 
-# In[3]:
 
-#### Implementacion de la parametrizacion 
+#### Implementacion de la parametrizacion
+#### Con parte del codigo de contour, mas la parametrizacion
 t=np.linspace(-np.pi,np.pi,300)
 xt=15**(1/4.)*np.sign(np.sin(t))*(np.sin(t)*np.sin(t))**(1/4.)
 yt=15**(1/4.)*np.sign(np.cos(t))*(np.cos(t)*np.cos(t))**(1/4.)
@@ -62,27 +59,20 @@ plt.ylim(-2.5,2.5)
 
 plt.title('Funciones F1 y F2 en nivel cero, con parametrizacion')
 
-
-# In[4]:
-
-## definimos la funcion 
+## definimos la funcion
+## para llevar la funcion F2 de dos variables (x,t), a una de 1 variable (t)
 
 def f2(to):
-    #t=np.linspace(-np.pi,np.pi,300.)
     xt=15**(1/4.)*np.sign(np.sin(to))*(np.sin(to)*np.sin(to))**(1/4.)
     yt=15**(1/4.)*np.sign(np.cos(to))*(np.cos(to)*np.cos(to))**(1/4.)
-    j=0
-    #e=2*np.pi/300
-    #for i in range(np.shape(t)[0]):
-    #    if abs(to-i)<=e:
-    #        j=i
     F2=xt**3*yt - xt*yt**3 - yt/2 - 1.937
     return F2
 
 
 
-a=scipy.optimize.bisect(f2,-2.2 , -2.3  )
+#Dado que no era obligatorio hacer un algortimo para en encontrar los intervalos de [a,b], no lo hice y los busque a mano con la siguiente "rutina"
 
+a=scipy.optimize.bisect(f2,-2.2 , -2.3  )
 t=np.linspace(-np.pi,np.pi,300.)
 punto=-2.2
 print 'funcion',f2(punto),'punto', punto,
