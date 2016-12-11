2bb6a1789d8e2f787442f080d799e6da1493802c
sergio <sergio.leivam@hotmail.com>
2016-12-11 14:02:02 -0300

    agrega repositorio tarea 12

---
+++ Tareas/12-tarea-sergioleivam/README.md
@@ -0,0 +1,5 @@
+# Tarea nro. 12
+
+Fecha de entrega 18/12/2016 23:59 hrs
+
+Enunciado en `enunciado.pdf`.
---
+++ Tareas/12-tarea-sergioleivam/Tarea-12.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from __future__ import division
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+
+from numpy import polyfit
+from scipy.optimize import curve_fit, leastsq
+from scipy.stats import cauchy, gamma, kstest, norm
